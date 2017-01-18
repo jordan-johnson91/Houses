aa2ca1bc3333ebed9486314b09414389b3ea7311
Jordan Johnson <jordan@wecancodeit.org>
2017-01-18 10:28:39 -0500

    Added a another constructor method

--- HousesExample/House.cs
+++ HousesExample/House.cs
@@ -13,6 +13,23 @@ namespace HousesExample
         private int size;
         private int doors;
 
+        public House(string color, int rooms,int size, int doors)
+        {
+            this.color = color;
+            this.rooms = rooms;
+            this.size = size;
+            this.doors = doors;
+
+
+
+        }
+
+        public House(string color, int size)
+        {
+            this.color = color;
+            this.size = size;
+
+        }
 
         public void Paint(string color)
         {
@@ -22,6 +39,14 @@ namespace HousesExample
         public string CurrentColor()
         {
             return color;
+        }
+
+        public void DisplayHouseInfo()
+        {
+            Console.Write("House Color: " + this.color + ",");
+            Console.Write(" with " + this.rooms + " rooms, ");
+            Console.Write(" Size of the house: " + this.size + "sq/ft,");
+            Console.WriteLine(" with " + this.doors + " doors");
         }
     }
 
--- HousesExample/Program.cs
+++ HousesExample/Program.cs
@@ -10,7 +10,11 @@ namespace HousesExample
     {
         static void Main(string[] args)
         {
+            House house1 = new House("green",5,9000,3);
 
+            
+            //Console.WriteLine(house1.CurrentColor());
+            house1.DisplayHouseInfo();
         }
     }
 }
